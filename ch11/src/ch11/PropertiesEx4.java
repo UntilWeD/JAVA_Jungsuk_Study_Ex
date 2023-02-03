cf5205ab1b5150583bdfebd72b27ccc0be7d63f9
Jhin Woo <muojeso90@gmail.com>
2023-02-03 13:28:06 +0900

    2023/02/03 644p~677p

---
+++ ch11/src/ch11/PropertiesEx4.java
@@ -0,0 +1,16 @@
+package ch11;
+
+import java.util.*;
+import java.io.*;
+
+
+public class PropertiesEx4 {
+
+	public static void main(String[] args) {
+		Properties sysProp = System.getProperties();
+		System.out.println("java.version :" + sysProp.getProperty("java.version"));
+		System.out.println("user.language :" + sysProp.getProperty("user.language"));
+		sysProp.list(System.out);
+	}
+
+}
