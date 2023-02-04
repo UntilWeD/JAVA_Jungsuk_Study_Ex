227ed4da4ea9d865e3556e7a76f6bfd9a10bd29c
Jhin Woo <muojeso90@gmail.com>
2023-02-04 16:22:31 +0900

    2023/02/04 702p~720p Finished 12Chapter

---
+++ ch12 Generics/src/AnnotationEx1.java
@@ -0,0 +1,21 @@
+
+public class AnnotationEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+class Parent{
+	void parentMethod() {}
+}		
+
+class Child extends Parent{
+//	@Override
+//	void parentmethod() {
+//		
+//	}
+}
