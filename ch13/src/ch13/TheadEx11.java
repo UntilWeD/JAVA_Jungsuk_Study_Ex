ba8d6ae0a0a1484eb630a6dbec4ba0fb2f8ba04b
Jhin Woo <muojeso90@gmail.com>
2023-02-06 19:13:33 +0900

    2023/02/06 728p~747p

---
+++ ch13/src/ch13/TheadEx11.java
@@ -0,0 +1,55 @@
+package ch13;
+
+import java.util.*;
+
+public class TheadEx11 {
+
+	public static void main(String[] args) {
+		Thread11_1 t1 = new Thread11_1("Thread1");
+		Thread11_2 t2 = new Thread11_2("Thread2");
+		
+		t1.start();
+		t2.start();
+	}
+
+}
+
+class Thread11_1 extends Thread {
+	public Thread11_1(String name) {
+		super(name);
+	}
+	
+	public void run() {
+		try {
+			sleep(5 * 1000);
+		} catch (InterruptedException e) {	}
+	}
+}
+
+class Thread11_2 extends Thread {
+	public Thread11_2(String name) {
+		super(name);
+	}
+	
+	public void run() {
+		Map map = getAllStackTraces();
+		Iterator it = map.keySet().iterator();
+		
+		int x =0;
+		while(it.hasNext()) {
+			Object obj = it.next();
+			Thread t= (Thread)obj;
+			StackTraceElement[] ste = (StackTraceElement[])(map.get(obj));
+			
+			System.out.println( "["+ ++x + "] name: " + t.getName()
+						+ ", group : " + t.getThreadGroup().getName()
+						+ ", daemon : " + t.isDaemon());
+			
+			for(int i=0; i < ste.length; i++) {
+				System.out.println(ste[i]);
+			}
+			
+			System.out.println();
+		}
+	}
+}
