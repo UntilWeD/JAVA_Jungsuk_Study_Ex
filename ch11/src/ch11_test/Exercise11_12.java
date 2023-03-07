824c2ee3fcdf6f362e8811cbc94fd9c8e412344f
Jhin Woo <muojeso90@gmail.com>
2023-03-08 00:32:03 +0900

    2023/03/07 ch11_test finshed

--- ch11/src/ch11_test/Exercise11_12.java
+++ ch11/src/ch11_test/Exercise11_12.java
@@ -1,135 +1,135 @@
-package ch11_test;
-
-import java.util.*;
-
-public class Exercise11_12 {
-	
-	public static void main(String[] args) throws Exception{
-		SutdaDeck deck = new SutdaDeck();
-		deck.shuffle();
-		
-		Player p1 = new Player("타짜", deck.pick(), deck.pick()); 
-		Player p2 = new Player("고수", deck.pick(), deck.pick());
-		
-		System.out.println(p1+" "+deck.getPoint(p1));
-		System.out.println(p2+" "+deck.getPoint(p2));
-	}
-}
-
-
-
-class SutdaDeck
-{
-	final int CARD_NUM = 20;
-	SutdaCard[] cards = new SutdaCard[CARD_NUM];
-	
-	int pos = 0; // 다음에 가져올 카드의 위치
-	HashMap jokbo = new HashMap(); // 족보를 저장할 HashMap
-	
-	SutdaDeck() {
-		for(int i=0;i < cards.length;i++) {
-			int num = i%10+1;
-			boolean isKwang = i < 10 && (num==1 || num==3 || num==8);
-			
-			cards[i] = new SutdaCard(num,isKwang);
-		}
-
-		registerJokbo(); // 족보를 등록한다.
-	}
-	
-	void registerJokbo() {
-		jokbo.put("KK", 4000);
-		int score = 3000;
-		for(int i = 10; i>0; i--) {
-			jokbo.put((i+"")+(i+""), score+(i*10));
-		}
-		
-		jokbo.put("12", 2060); jokbo.put("14", 2050); jokbo.put("19", 2040);
-		jokbo.put("21", 2060); jokbo.put("41", 2050); jokbo.put("91", 2040);
-		jokbo.put("110", 2030); jokbo.put("410", 2020); jokbo.put("46", 2010);
-		jokbo.put("101", 2030); jokbo.put("104", 2020); jokbo.put("64", 2010);
-	}
-	
-	int getPoint(Player p) {
-		if(p==null) return 0;
-		
-		SutdaCard c1 = p.c1;
-		SutdaCard c2 = p.c2;
-		
-		Integer result = 0;
-		
-		if(c1.isKwang && c2.isKwang) {
-			result =Integer.parseInt((jokbo.get("KK")).toString());
-		} else if ((jokbo.get((" "+c1.num) + (" "+c2.num))) != null) {
-			result = Integer.parseInt(jokbo.get((" "+c1.num) + (" "+c2.num)).toString());
-		} else {
-			result = (c1.num + c2.num) % 10 + 1000;
-		}
-		
-		return result.intValue();
-	}
-	
-	SutdaCard pick() throws Exception {
-		SutdaCard c = null;
-		
-		if(0 <= pos && pos < CARD_NUM) {
-			c = cards[pos];
-			cards[pos++] = null;
-		} else {
-			throw new Exception("남아있는 카드가 없습니다."); 
-		}
-			return c;
-	}
-	
-	void shuffle() {
-		for(int x=0; x < CARD_NUM * 2; x++) {
-			int i = (int)(Math.random() * CARD_NUM);
-			int j = (int)(Math.random() * CARD_NUM);
-			
-			SutdaCard tmp = cards[i];
-			cards[i] = cards[j];
-			cards[j] = tmp;
-		}
-	}
-} // SutdaDeck
-
-class Player {
-	String name;
-	SutdaCard c1;
-	SutdaCard c2;
-	
-	int point; // 카드의 등급에 따른 점수 - 새로 추가
-	
-	Player(String name, SutdaCard c1, SutdaCard c2) {
-		this.name = name ;
-		this.c1 = c1 ;
-		this.c2 = c2 ;
-	}
-	
-	public String toString() {
-		return "["+name+"]"+ c1.toString() +","+ c2.toString();
-	}
-	
-} // class Player
-
-class SutdaCard {
-	int num;
-	boolean isKwang;
-	
-	SutdaCard() {
-		this(1, true);
-	}
-	
-	SutdaCard(int num, boolean isKwang) {
-		this.num = num;
-		this.isKwang = isKwang;
-	}
-	
-	public String toString() {
-		return num + ( isKwang ? "K":"");
-	}
-}
-
+//package ch11_test;
+//
+//import java.util.*;
+//
+//public class Exercise11_12 {
+//	
+//	public static void main(String[] args) throws Exception{
+//		SutdaDeck deck = new SutdaDeck();
+//		deck.shuffle();
+//		
+//		Player p1 = new Player("타짜", deck.pick(), deck.pick()); 
+//		Player p2 = new Player("고수", deck.pick(), deck.pick());
+//		
+//		System.out.println(p1+" "+deck.getPoint(p1));
+//		System.out.println(p2+" "+deck.getPoint(p2));
+//	}
+//}
+//
+//
+//
+//class SutdaDeck
+//{
+//	final int CARD_NUM = 20;
+//	SutdaCard[] cards = new SutdaCard[CARD_NUM];
+//	
+//	int pos = 0; // 다음에 가져올 카드의 위치
+//	HashMap jokbo = new HashMap(); // 족보를 저장할 HashMap
+//	
+//	SutdaDeck() {
+//		for(int i=0;i < cards.length;i++) {
+//			int num = i%10+1;
+//			boolean isKwang = i < 10 && (num==1 || num==3 || num==8);
+//			
+//			cards[i] = new SutdaCard(num,isKwang);
+//		}
+//
+//		registerJokbo(); // 족보를 등록한다.
+//	}
+//	
+//	void registerJokbo() {
+//		jokbo.put("KK", 4000);
+//		int score = 3000;
+//		for(int i = 10; i>0; i--) {
+//			jokbo.put((i+"")+(i+""), score+(i*10));
+//		}
+//		
+//		jokbo.put("12", 2060); jokbo.put("14", 2050); jokbo.put("19", 2040);
+//		jokbo.put("21", 2060); jokbo.put("41", 2050); jokbo.put("91", 2040);
+//		jokbo.put("110", 2030); jokbo.put("410", 2020); jokbo.put("46", 2010);
+//		jokbo.put("101", 2030); jokbo.put("104", 2020); jokbo.put("64", 2010);
+//	}
+//	
+//	int getPoint(Player p) {
+//		if(p==null) return 0;
+//		
+//		SutdaCard c1 = p.c1;
+//		SutdaCard c2 = p.c2;
+//		
+//		Integer result = 0;
+//		
+//		if(c1.isKwang && c2.isKwang) {
+//			result =Integer.parseInt((jokbo.get("KK")).toString());
+//		} else if ((jokbo.get((" "+c1.num) + (" "+c2.num))) != null) {
+//			result = Integer.parseInt(jokbo.get((" "+c1.num) + (" "+c2.num)).toString());
+//		} else {
+//			result = (c1.num + c2.num) % 10 + 1000;
+//		}
+//		
+//		return result.intValue();
+//	}
+//	
+//	SutdaCard pick() throws Exception {
+//		SutdaCard c = null;
+//		
+//		if(0 <= pos && pos < CARD_NUM) {
+//			c = cards[pos];
+//			cards[pos++] = null;
+//		} else {
+//			throw new Exception("남아있는 카드가 없습니다."); 
+//		}
+//			return c;
+//	}
+//	
+//	void shuffle() {
+//		for(int x=0; x < CARD_NUM * 2; x++) {
+//			int i = (int)(Math.random() * CARD_NUM);
+//			int j = (int)(Math.random() * CARD_NUM);
+//			
+//			SutdaCard tmp = cards[i];
+//			cards[i] = cards[j];
+//			cards[j] = tmp;
+//		}
+//	}
+//} // SutdaDeck
+//
+//class Player {
+//	String name;
+//	SutdaCard c1;
+//	SutdaCard c2;
+//	
+//	int point; // 카드의 등급에 따른 점수 - 새로 추가
+//	
+//	Player(String name, SutdaCard c1, SutdaCard c2) {
+//		this.name = name ;
+//		this.c1 = c1 ;
+//		this.c2 = c2 ;
+//	}
+//	
+//	public String toString() {
+//		return "["+name+"]"+ c1.toString() +","+ c2.toString();
+//	}
+//	
+//} // class Player
+//
+//class SutdaCard {
+//	int num;
+//	boolean isKwang;
+//	
+//	SutdaCard() {
+//		this(1, true);
+//	}
+//	
+//	SutdaCard(int num, boolean isKwang) {
+//		this.num = num;
+//		this.isKwang = isKwang;
+//	}
+//	
+//	public String toString() {
+//		return num + ( isKwang ? "K":"");
+//	}
+//}
+//
 
 
 
--- ch11/src/ch11_test/Exercise11_9.java
+++ ch11/src/ch11_test/Exercise11_9.java
@@ -1,140 +1,140 @@
-package ch11_test;
-
-import java.util.*;
-
-class Student implements Comparable {
-	String name;
-	int ban;
-	int no;
-	int kor;
-	int eng;
-	int math;
-	int total;
-	int schoolRank; // 전교등수
-	int classRank; // 반등수
-	
-	Student(String name, int ban, int no, int kor, int eng, int math) {
-		this.name = name;
-		this.ban = ban;
-		this.no = no;
-		this.kor = kor;
-		this.eng = eng;
-		this.math = math;
-		total = kor+eng+math;
-	}
-	
-	int getTotal() {
-		return total;
-	}
-	float getAverage() {
-		return (int)((getTotal()/ 3f)*10+0.5)/10f;
-	}
-	
-	public int compareTo(Object o) {
-		if(o instanceof Student) {
-			Student tmp = (Student)o;
-			return tmp.total - this.total;
-		} else {
-			return -1;
-		}
-	}
-	
-	public String toString() {
-		return name
-		+","+ban
-		+","+no
-		+","+kor
-		+","+eng
-		+","+math
-		+","+getTotal()
-		+","+getAverage()
-		+","+schoolRank
-		+","+classRank // 새로추가
-		;
-	}
-} // class Student
-
-class ClassTotalComparator implements Comparator {
-	public int compare(Object o1, Object o2) {
-		if(o1 instanceof Comparable && o2 instanceof Comparable) {
-			Student stu1 = (Student)o1;
-			Student stu2 = (Student)o2;
-			return stu1.ban > stu2.ban ? 1 : (stu1.ban == stu2.ban ? stu1.compareTo(stu2) : -1);
-		}
-		return -1;
-	}
-}
-
-public class Exercise11_9 {
-	
-	public static void calculateClassRank(List list) {
-		//먼저 반별 총점기준 내림차순으로 정렬한다.
-		Collections.sort(list, new ClassTotalComparator());
-		
-		int prevBan = -1;
-		int prevRank = -1;
-		int prevTotal = -1;
-		int length = list.size();
-		
-		for(int i = 0; i< length; i++) {
-			Student stu = (Student)(list.get(i));
-			
-			if(stu.ban != prevBan) {
-				prevRank = 1;
-				prevTotal = 1;
-				stu.classRank = prevRank;
-			} else if (stu.total == prevTotal) {
-				stu.classRank = prevRank;
-			} else if (stu.total != prevTotal) {
-				stu.classRank = ++prevRank;
-			}
-			
-			prevBan = stu.ban;
-			prevTotal = stu.total;
-			prevRank = stu.classRank;
-		}
-		
-		
-	}
-	
-	public static void calculateSchoolRank(List list) {
-		Collections.sort(list); 
-		
-		int prevRank = -1; // 이전 전교등수
-		int prevTotal = -1; // 이전 총점
-		int length = list.size();
-		
-		Student stu = null;
-		
-		for(int i = 0; i< length; i++) {
-			stu = (Student)(list.get(i));
-			
-			if(stu.getTotal() == prevTotal) {
-				stu.schoolRank = prevRank;
-			} else if (stu.getTotal() != prevTotal) {
-				stu.schoolRank = i+1;
-			}
-			
-			prevTotal = stu.getTotal();
-			prevRank = stu.schoolRank;
-		}
-	}
-
-	public static void main(String[] args) {
-		ArrayList list = new ArrayList();
-		
-		list.add(new Student("이자바",2,1,70,90,70)); 
-		list.add(new Student("안자바",2,2,60,100,80)); 
-		list.add(new Student("홍길동",1,3,100,100,100)); 
-		list.add(new Student("남궁성",1,1,90,70,80)); 
-		list.add(new Student("김자바",1,2,80,80,90));
-		
-		calculateSchoolRank(list);
-		calculateClassRank(list);
-		
-		Iterator it = list.iterator();
-		
-		while(it.hasNext())
-			System.out.println(it.next());
-	}
-}
+//package ch11_test;
+//
+//import java.util.*;
+//
+//class Student implements Comparable {
+//	String name;
+//	int ban;
+//	int no;
+//	int kor;
+//	int eng;
+//	int math;
+//	int total;
+//	int schoolRank; // 전교등수
+//	int classRank; // 반등수
+//	
+//	Student(String name, int ban, int no, int kor, int eng, int math) {
+//		this.name = name;
+//		this.ban = ban;
+//		this.no = no;
+//		this.kor = kor;
+//		this.eng = eng;
+//		this.math = math;
+//		total = kor+eng+math;
+//	}
+//	
+//	int getTotal() {
+//		return total;
+//	}
+//	float getAverage() {
+//		return (int)((getTotal()/ 3f)*10+0.5)/10f;
+//	}
+//	
+//	public int compareTo(Object o) {
+//		if(o instanceof Student) {
+//			Student tmp = (Student)o;
+//			return tmp.total - this.total;
+//		} else {
+//			return -1;
+//		}
+//	}
+//	
+//	public String toString() {
+//		return name
+//		+","+ban
+//		+","+no
+//		+","+kor
+//		+","+eng
+//		+","+math
+//		+","+getTotal()
+//		+","+getAverage()
+//		+","+schoolRank
+//		+","+classRank // 새로추가
+//		;
+//	}
+//} // class Student
+//
+//class ClassTotalComparator implements Comparator {
+//	public int compare(Object o1, Object o2) {
+//		if(o1 instanceof Comparable && o2 instanceof Comparable) {
+//			Student stu1 = (Student)o1;
+//			Student stu2 = (Student)o2;
+//			return stu1.ban > stu2.ban ? 1 : (stu1.ban == stu2.ban ? stu1.compareTo(stu2) : -1);
+//		}
+//		return -1;
+//	}
+//}
+//
+//public class Exercise11_9 {
+//	
+//	public static void calculateClassRank(List list) {
+//		//먼저 반별 총점기준 내림차순으로 정렬한다.
+//		Collections.sort(list, new ClassTotalComparator());
+//		
+//		int prevBan = -1;
+//		int prevRank = -1;
+//		int prevTotal = -1;
+//		int length = list.size();
+//		
+//		for(int i = 0; i< length; i++) {
+//			Student stu = (Student)(list.get(i));
+//			
+//			if(stu.ban != prevBan) {
+//				prevRank = 1;
+//				prevTotal = 1;
+//				stu.classRank = prevRank;
+//			} else if (stu.total == prevTotal) {
+//				stu.classRank = prevRank;
+//			} else if (stu.total != prevTotal) {
+//				stu.classRank = ++prevRank;
+//			}
+//			
+//			prevBan = stu.ban;
+//			prevTotal = stu.total;
+//			prevRank = stu.classRank;
+//		}
+//		
+//		
+//	}
+//	
+//	public static void calculateSchoolRank(List list) {
+//		Collections.sort(list); 
+//		
+//		int prevRank = -1; // 이전 전교등수
+//		int prevTotal = -1; // 이전 총점
+//		int length = list.size();
+//		
+//		Student stu = null;
+//		
+//		for(int i = 0; i< length; i++) {
+//			stu = (Student)(list.get(i));
+//			
+//			if(stu.getTotal() == prevTotal) {
+//				stu.schoolRank = prevRank;
+//			} else if (stu.getTotal() != prevTotal) {
+//				stu.schoolRank = i+1;
+//			}
+//			
+//			prevTotal = stu.getTotal();
+//			prevRank = stu.schoolRank;
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		ArrayList list = new ArrayList();
+//		
+//		list.add(new Student("이자바",2,1,70,90,70)); 
+//		list.add(new Student("안자바",2,2,60,100,80)); 
+//		list.add(new Student("홍길동",1,3,100,100,100)); 
+//		list.add(new Student("남궁성",1,1,90,70,80)); 
+//		list.add(new Student("김자바",1,2,80,80,90));
+//		
+//		calculateSchoolRank(list);
+//		calculateClassRank(list);
+//		
+//		Iterator it = list.iterator();
+//		
+//		while(it.hasNext())
+//			System.out.println(it.next());
+//	}
+//}
